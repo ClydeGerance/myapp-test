909e569c6cdb0d7e06d903fc288c87633c8b6d6c
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-05-29 21:36:44 +0800

    initial commit

---
+++ myapp/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exploration!',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App Learning'),
+      ),
+      body: Column(
+        children: [
+          Progress(),
+          TaskList(),
+        ],
+      ),
+    );
+  }
+}
+
+class Progress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('You are this far away from being a hackergod:'),
+        LinearProgressIndicator(value: 0.0),
+      ],
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TaskItem(label: 'Install Flutter'),
+        TaskItem(label: 'Install Android Studio'),
+        TaskItem(label: 'Create Emulator'),
+        TaskItem(label: 'Ask phind to create a code'),
+        TaskItem(label: 'Adjust the code to make it look like you did it'),
+      ],
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  final String label;
+
+  TaskItem({Key? key, required this.label}) : super(key: key);
+
+  @override
+  _TaskItemState createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  bool? _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          onChanged: (newValue) => setState(() => _value = newValue),
+          value: _value,
+        ),
+        Text(widget.label),
+      ],
+    );
+  }
+}
